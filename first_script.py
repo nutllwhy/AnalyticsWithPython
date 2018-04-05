e459a3c9553fb31d711c6dbd3c48d9b043c180f9
nutllwhy <linli0301@hotmal.com>
2018-04-06 00:28:41 +0800

    pp.8

---
+++ first_script.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+print("Output #1: I'm excited to learn Python.")
+
+#add two number
+x = 4
+y = 5
+z = x + y
+print("Output #2: Four plus five equals {0:d}.".format(z))
+
+#add two list
+a = [1, 2, 3, 4]
+b = ["first", "second", "third", "fourth"]
+c = a + b
+print("Output #3: {0}, {1}, {2}". format(a, b, c))
