5b8fc29fe6a1e181ce47c524517a59545c72e555
nutllwhy <linli0301@hotmal.com>
2018-04-07 23:22:36 +0800

    pp.12

--- first_script.py
+++ first_script.py
@@ -7,7 +7,7 @@ x = 4
 y = 5
 z = x + y
 print("Output #2: Four plus five equals {0:d}.".format(z))
-
+ 
 #add two list
 a = [1, 2, 3, 4]
 b = ["first", "second", "third", "fourth"]
@@ -25,3 +25,18 @@ print("Output #8: {0: .1f}".format(y))
 r = 8/float(3)
 print("Output #9: {0: .2f}".format(r))
 print("Output #10: {0: .4f}".format(8.0/3))
+
+print("Output #11: {0: .4f}".format(exp(3)))
+print("Output #12: {0: .2f}".format(log(4)))
+print("Output #13: {0: .1f}".format(sqrt(81)))
+
+print("Output #14: {0: s}".format('I\'m enjoying learning Python.'))
+print("Output #15: {0: s}".format("This is a long string. Without the backslash\
+	it would run off of the page on the right in the text editor and be very\
+	difficult to read and edit. By using the backslash you can split the long\
+	string into smaller strings on separate lines so that the whole string is easy\
+	to view in the text editor."))
+print("Output #16: {0: s}".format('''You can use triple single quetes
+	for multi-line comment string.'''))
+print("Output #17: {0: s}".format("""You can also use triple double quotes
+	for multi-line comment string."""))
