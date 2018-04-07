612fe0c869ff6c4020607add27e4164e0415c9e1
nutllwhy <linli0301@hotmal.com>
2018-04-08 01:03:14 +0800

    fix the bug

--- first_script.py
+++ first_script.py
@@ -30,13 +30,14 @@ print("Output #11: {0: .4f}".format(exp(3)))
 print("Output #12: {0: .2f}".format(log(4)))
 print("Output #13: {0: .1f}".format(sqrt(81)))
 
-print("Output #14: {0: s}".format('I\'m enjoying learning Python.'))
-print("Output #15: {0: s}".format("This is a long string. Without the backslash\
+#print("Output #14: {0: s}".format('I\'m enjoying learning Python.'))
+print("Output #14: {0:s}".format('I\'m enjoying learning Python.'))
+print("Output #15: {0:s}".format("This is a long string. Without the backslash\
 	it would run off of the page on the right in the text editor and be very\
 	difficult to read and edit. By using the backslash you can split the long\
 	string into smaller strings on separate lines so that the whole string is easy\
 	to view in the text editor."))
-print("Output #16: {0: s}".format('''You can use triple single quetes
+print("Output #16: {0:s}".format('''You can use triple single quetes
 	for multi-line comment string.'''))
-print("Output #17: {0: s}".format("""You can also use triple double quotes
+print("Output #17: {0:s}".format("""You can also use triple double quotes
 	for multi-line comment string."""))
