5ee1d10cdc163ed4ff3cefe494080e8cc8597539
nutllwhy <linli0301@hotmail.com>
2018-04-12 17:48:34 +0800

    pp.16

--- first_script.py
+++ first_script.py
@@ -50,14 +50,47 @@
 # m = len(sentence)
 # print("Output #20: {0:d}".format(m))
 
-string1 = "My deliverable is due in May"
-string1_list1 = string1.split()
-string1_list2 = string1.split(" ", 2)
-print("Output #21:{0}".format(string1_list1))
-print("Output #22: FIRST PIECE:{0}, SECOND PIECE:{1}, THIRD PIECE:{2}"\
-      .format(string1_list2[0], string1_list2[1], string1_list2[2]))
-string2 = "Your,deliverable,is,due,in,June"
-string2_list = string2.split(',')
-print("Output #23: {0}".format(string2_list))
-print("Output #24: {0}{1}{2}".format(string2_list[1], string2_list[5],\
-                                      string2_list[-1]))
+# string1 = "My deliverable is due in May"
+# string1_list1 = string1.split()
+# string1_list2 = string1.split(" ", 2)
+# print("Output #21:{0}".format(string1_list1))
+# print("Output #22: FIRST PIECE:{0}, SECOND PIECE:{1}, THIRD PIECE:{2}"\
+#       .format(string1_list2[0], string1_list2[1], string1_list2[2]))
+# string2 = "Your,deliverable,is,due,in,June"
+# string2_list = string2.split(',')
+# print("Output #23: {0}".format(string2_list))
+# print("Output #24: {0}{1}{2}".format(string2_list[1], string2_list[5],\
+#                                       string2_list[-1]))
+
+# string3 = " Remove unwanted characters   from this string.\t\t  \n"
+# print("Output #26:string3: {0:s}".format(string3))
+# string3_lstrip = string3.lstrip()
+# print("Output #27: lstrip: {0:s}".format(string3_lstrip))
+# #string3_rstrip = string3.rsplit()
+# #print("Output #28: rstrip: {0:s}".format(string3_rstrip))
+# string3_strip = string3.strip()
+# print("Output #29: strip: {0:s}".format(string3_strip))
+# print("END")
+#
+# string4 = "$$Here's another string that has unwanted characters.___---++"
+# print("Output #30: {0:s}".format(string4))
+# string4 = "$$The unwanted characters have been removed.__---++"
+# string4_strip = string4.strip('$_-+')
+# print("Output #31: {0:s}".format(string4_strip))
+
+# string5 = "Let's replace the spaces in this sentence with other characters."
+# string5_replace = string5.replace(" ", "!@!")
+# print("Output #32 (with !@!): {0:s}".format(string5_replace))
+# string5_replace = string5.replace(" ", ",")
+# print("Output #33 (with commas): {0:s}".format(string5_replace))
+
+string6 = "Here's WHAT Happens WHEN You Use lower."
+print("Output #34: {0:s}".format(string6.lower()))
+string7 = "Here's what Happens when You Use UPPER."
+print("Output #35: {0:s}".format(string7.upper()))
+string5 = "here's WHAT Happens WHEN you use Capitalize."
+print("Output #36: {0:s}".format(string5.capitalize()))
+string5_list = string5.split()
+print("Output #37 (on each word):")
+for word in string5_list:
+    print("{0:s}".format(word.capitalize()))
